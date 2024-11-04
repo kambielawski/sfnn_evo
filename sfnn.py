62d1a1cffc6d4e5fdabb5c9a5645b74b350507f8
Ahmad Arrabi <ahmadzahiarrabi@gmail.com>
2024-11-03 23:24:32 -0500

    SFNN mostly done\nJust need to revise the forward pass logic and ticks ... some for loops still need to be replaced with matrix tensor operations. Current implementatio is really simple (stupid in a way)  but should work as a 'simplest SFNN' design, we can optimize it from here :D \nNOTE: I'll add some comments and make it more readable and less mentally exhaustive

--- sfnn.py
+++ sfnn.py
@@ -35,18 +35,16 @@ class Synapse(nn.Module):
         - hidden_size: hidden state of GRU (output of GRU)
     """
     def __init__(self, input_size, hidden_size):
-        #TODO: MAY NEED TO MODIFY THIS TO MAKE IT ACCEPT A LAYER HIDDEN STATE
         super().__init__()
 
-        self.Gru = nn.GRU(input_size=input_size, hidden_size=hidden_size)
-        self.hidden_state = torch.randn(hidden_size) #MAY NEED TO CHANGE BASED ON TYPE OF SYNAPSE!
-        self.lr = nn.Parameter(torch.tensor(0.01))
-
-    def forward(self, pre_neurons, post_neurons, reward):
+        self.Gru = nn.GRUCell(input_size=input_size, hidden_size=hidden_size)
+        
+    def forward(self, pre_neurons, post_neurons, reward, hidden_state, lr):
         Gru_input = torch.cat([pre_neurons, post_neurons, reward])
-        self.hidden_state = self.hidden_state + self.Gru(Gru_input, self.hidden_state)*self.lr
-        post_neurons = self.hidden_state*pre_neurons
-        return post_neurons
+        Gru_output = self.Gru(Gru_input, hidden_state)
+        updated_hidden_state = hidden_state + Gru_output*lr
+        Synapse_output = updated_hidden_state*pre_neurons
+        return Synapse_output, updated_hidden_state
     
 class SFNN(nn.Module):
     """
@@ -58,12 +56,12 @@ class SFNN(nn.Module):
         - hidden_layer_size: # of neurons in hidden layer
         - ouput_layer_size: # of neurons in output layer
     """
-    def __init__(self, neuron_size, input_layer_size, hidden_layer_size, output_layer_size):
+    def __init__(self, neuron_size, input_layer_size, hidden_layer_size, output_layer_size, lr):
         super().__init__()
 
         self.input_layer_neuron = Neuron(neuron_size=neuron_size)
         self.hidden_layer_neuron = Neuron(neuron_size=neuron_size)
-        self.outout_layer_neuron = Neuron(neuron_size=neuron_size)
+        self.output_layer_neuron = Neuron(neuron_size=neuron_size)
         
         self.input_layer_synapse = Synapse(input_size=neuron_size*2+1, hidden_size=neuron_size)
         self.hidden_layer_synapse = Synapse(input_size=neuron_size*2+1, hidden_size=neuron_size)
@@ -72,17 +70,27 @@ class SFNN(nn.Module):
         self.input_layer_size = input_layer_size
         self.hidden_layer_size = hidden_layer_size
         self.output_layer_size = output_layer_size
+        self.total_neurons = input_layer_size + hidden_layer_size + output_layer_size
+        
+        self.neuron_size = neuron_size
 
         self.init_sparsity_matrix()
-        #TODO: add placeholder for synapse states at each edge in Adjacency matrix
+        self.init_hidden_state_matrix()
+        self.init_post_neuron_state_matrix()
+        self.lr = nn.Parameter(torch.tensor(lr))
+
+    def set_parameters(self, parameters):
+        self.load_state_dict(parameters)
+
+    def get_parameters(self):
+        return self.state_dict()
 
     def init_sparsity_matrix(self):
         """
         initialize nxn adjacency matrix
         This implementation assumes the matrix is ordered as follows [input, hidden, output]
         """
-        Total_neurons = self.input_layer_size + self.hidden_layer_size + self.output_layer_size
-        self.Adjacency_matrix = torch.bernoulli(torch.ones(Total_neurons, Total_neurons)/2)
+        self.Adjacency_matrix = torch.bernoulli(torch.ones(self.total_neurons, self.total_neurons)/2)
         self.vallidate_Adjacency_matrix()
 
     def vallidate_Adjacency_matrix(self):
@@ -99,11 +107,65 @@ class SFNN(nn.Module):
         self.Adjacency_matrix[self.input_layer_size+self.hidden_layer_size:, :self.input_layer_size] = 0
         self.Adjacency_matrix[self.input_layer_size+self.hidden_layer_size:, self.input_layer_size+self.hidden_layer_size:] = 0
 
-    def forward(self, x):
-        #TODO: calculate the forward pass according to the adjacency matrix
-        #      you will probably need to safe the states of synapses in some dict or matrix!!!
-        pass
+    def init_hidden_state_matrix(self):
+        self.hidden_state = torch.randn(self.total_neurons, self.total_neurons, self.neuron_size)
 
-    def get_parameters(self):
-        # TODO: return list of parameters to be optimized in a neat way alligned with evo.py
-        pass
+    def init_post_neuron_state_matrix(self):
+        self.post_neuron_state = torch.randn(self.total_neurons, self.neuron_size)
+    
+    def forward(self, obs, reward):
+
+        #output placeholder
+        placeholder = torch.zeros(self.total_neurons, self.total_neurons, self.neuron_size)
+        
+        #create observation tensor
+        input_signal = obs.reshape(self.input_layer_size, 1).expand(self.input_layer_size, self.neuron_size)
+        
+        #process input FC layer
+        self.post_neuron_state[:self.input_layer_size] = self.input_layer_neuron(input_signal) # [input_layer_size, neuron_size]
+
+        #process input synapse
+        for i in range(self.input_layer_size):
+            connected_hidden_neurons = (self.Adjacency_matrix[i] == 1).nonzero(as_tuple=True)[0].tolist()
+            for j in connected_hidden_neurons:
+                placeholder[i,j], self.hidden_state[i,j] = self.input_layer_synapse(self.post_neuron_state[i],
+                                                                          self.post_neuron_state[j],
+                                                                          reward,
+                                                                          self.hidden_state[i,j],
+                                                                          self.lr)
+
+        #process hidden FC
+        self.post_neuron_state[self.input_layer_size:self.input_layer_size+self.hidden_layer_size] = self.hidden_layer_neuron(placeholder[:, self.input_layer_size:self.input_layer_size+self.hidden_layer_size].sum(axis=0))
+
+        #process hidden synapse
+        for i in range(self.hidden_layer_size):
+            relative_i = self.input_layer_size + i # relative position with offset
+            connected_neurons = (self.Adjacency_matrix[relative_i] == 1).nonzero(as_tuple=True)[0].tolist()
+            for j in connected_neurons:
+                placeholder[relative_i, j], self.hidden_state[relative_i,j] = self.hidden_layer_synapse(self.post_neuron_state[relative_i],
+                                                                      self.post_neuron_state[j],
+                                                                      reward,
+                                                                      self.hidden_state[relative_i,j], 
+                                                                      self.lr)
+
+        #process output FC
+        self.post_neuron_state[self.input_layer_size+self.hidden_layer_size:] = self.output_layer_neuron(placeholder[:, self.input_layer_size+self.hidden_layer_size:].sum(axis=0))
+
+        #process ouput (softmax over the first element of each out neuron) 
+        for i in range(self.output_layer_size):
+            relative_i = i + self.input_layer_size + self.hidden_layer_size
+            connected_neurons = (self.Adjacency_matrix[relative_i] == 1).nonzero(as_tuple=True)[0].tolist()
+            for j in connected_neurons:
+                placeholder[relative_i, j], self.hidden_state[relative_i,j] = self.output_layer_synapse(self.post_neuron_state[relative_i],
+                                                                      self.post_neuron_state[j],
+                                                                      reward,
+                                                                      self.hidden_state[relative_i,j], 
+                                                                      self.lr)
+                
+        return self.post_neuron_state[-self.output_layer_size:, 0].argmax()
+
+if __name__=='__main__':
+    model = SFNN(input_layer_size=2, hidden_layer_size=4, output_layer_size=2, neuron_size=4, lr=0.1)
+    obs = torch.tensor([5,0.5])
+    reward = torch.tensor([5])
+    print(model(obs, reward))
