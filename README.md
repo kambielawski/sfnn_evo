44c2b37db812cd3450c0e46cbc702e25a6a32e6c
Kameron Bielawski <kam@ip0af5c96f.int.uvm.edu>
2024-11-12 15:15:30 -0500

    Running experiments

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Evolution of Structurally Flexible NNs
+
+**evo.py** -  Evolutionary algorithms; fitness functions; mutation and crossover operations
+
+**sffn.py** - Torch implementation of Structurally Flexible NN
+
+**rl.py** - Implementation of RL environment and evaluation of a SFNN
+
+## Running an experiment
+
+1. Create an experiment file
+
+Example:
+
+```
+{
+    "ea": "HillClimber",
+    "n_runs": 1,
+    "evo_parameters": {
+        "population_size": 10,
+        "n_generations": 1000,
+        "mutation_rate": 0.01,
+        "neuron_size": 10,
+        "gru_size": 10,
+        "hidden_layer_size": 10,
+        "lr": 0.1,
+        "ticks": 2
+    }
+}
+```
+
+2. Run `run_exp.py` and pass in the experiment file as a CLI parameter
+
+Example:
+```
+python3 run_exp.py exp_file_example.json
+```
---
+++ run_exp.py
@@ -0,0 +1,34 @@
+"""
+Run an experiment
+"""
+import os
+import argparse
+import pickle
+import json
+from datetime import datetime
+from evo import HillClimber
+
+# Parse CLI arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("exp_file", type=str, help="Path to the experiment file")
+args = parser.parse_args()
+
+# Create experiment directory
+os.makedirs('experiments', exist_ok=True)
+date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+exp_dir = f'./experiments/exp_{date}'
+os.makedirs(exp_dir, exist_ok=True)
+
+# Load experiment configuration
+with open(args.exp_file, "r") as f:
+    exp_config = json.load(f)
+
+# Run experiment
+if __name__ == "__main__":
+    if exp_config["ea"] == "HillClimber":
+        ea = HillClimber(**exp_config["evo_parameters"])
+        ea.evolve()
+        with open(os.path.join(exp_dir, 'ea.pkl'), 'wb') as f:
+            pickle.dump(ea, f)
+    else:   
+        raise ValueError(f"Invalid EA: {exp_config['ea']}")
