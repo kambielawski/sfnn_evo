217991233a3ce14035087927ee8827ae2ecfc648
Kameron Bielawski <kam@EMCFs-MacBook-Pro-4.local>
2024-11-20 09:18:21 -0500

    vacc params

--- bluemoon_submit.sh
+++ bluemoon_submit.sh
@@ -3,12 +3,15 @@
 #SBATCH --partition=bluemoon
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gpus=1
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=32
 #SBATCH --job-name=sfnn
 #SBATCH --output=./vacc_out_files/%x_%j.out
 #SBATCH --time=8:00:00
 
 set -x
+
+conda activate sfnn-evo
 
 echo $1
 echo $2
